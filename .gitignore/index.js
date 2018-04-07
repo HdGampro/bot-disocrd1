4d3de80a8e8010c3c3e8e295702bcc9110aeb498
HdGampro <38081585+HdGampro@users.noreply.github.com>
2018-04-07 23:00:43 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+bot.login('NDMyMjYwOTE0MjQzMzA1NDgy.DaquNA.50Go60_aLkhcv_XnHqrZEcDSVEs');
+bot.on('ready', function() {
+    console.log("I'm ready !");
+});
+
+bot.on("message", (message) => {
+
+    // Configs
+    const PREFIX = ("/");
+    if (!message.content.startsWith(PREFIX) || message.guild === null) return;
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+
+    // Commande /hello
+    if(command === "hello"){
+        message.reply('Salut !');
+     }else if(command === "help"){
+        message.reply("page d'aide affichée avec sucès");
+     }else if(command === "test"){
+         message.reply("test de connection avec le bot reussi !")
+     }
+
+});
+
+bot.on("guildMemberAdd", (member) => {
+    member.guild.channels.find("name", "bienvenue").send(`bienvenue ${member} n'hésite pas à invité tes amis pour obtenir le grade @V.I.P`)
+
+});
